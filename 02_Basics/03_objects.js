3249b66c95c63627e5a978291fb859c59a696498
Ravi Karmakar <ravikarmkar94475@gmail.com>
2024-11-19 14:13:04 +0530

    fundamentlas of objects and functions

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -1,8 +1,14 @@
+// singleton
+// Object.create
+
+// object literals
+
 const jsSym = Symbol("jsRavi");
 
 let jsUser = {
   name: "Ravi",
   [jsSym]: "rohan",
+  email: "ravi@gmail.com",
   "last-name": "Karmakar",
   "full name": "Ravi Karmakar",
   lang: "JavaScript",
@@ -14,7 +20,23 @@ let jsUser = {
 };
 
 // console.log(jsUser);
-console.log(jsUser.name);
-console.log(jsUser["lang"]);
-console.log(jsUser["last-name"]);
-console.log(jsUser[jsSym]);
+// console.log(jsUser.name);
+// console.log(jsUser["lang"]);
+// console.log(jsUser["last-name"]);
+// console.log(jsUser[jsSym]);
+
+// jsUser.email = "ravi@newgmail.com";
+// Object.freeze(jsUser);
+// jsUser["email"] = "ravi@newgmail2.com";
+// console.log(jsUser.email);
+// console.log(jsUser);
+
+jsUser.greeting = function () {
+  console.log("Hello New User");
+};
+
+jsUser.greetingTow = function () {
+  console.log(`Hello New User ${this.name}`);
+};
+
+console.log(jsUser.greeting(), jsUser.greetingTow());
