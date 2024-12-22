6669a6c5b1b0e65d515861621844c1bf22dc3edb
Ravi Karmakar <ravikarmkar94475@gmail.com>
2024-12-22 10:52:01 +0530

    all about basics of loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+let index = 1;
+
+while (index <= 10) {
+  // console.log(index);
+  index = index + 3;
+}
+
+let superHero = ["spiderman", "thor", "ironman", "hulk"];
+
+let arr = 0;
+
+while (arr < superHero.length) {
+  // console.log(superHero[arr]);
+  arr = arr + 1;
+}
+
+// let score = 11;
+let score = 1;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
